39d40d6e663fb2c388e43e13af00e259089531df
Ussayed Shakeel <TTS72I@tr.pte.hu>
2023-08-06 15:24:50 +0200

    Update edit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override');
 
 const Campground = require('./models/campground')
 
@@ -18,8 +19,9 @@ db.once("open",()=>{
 
 const app = express();
 
-app.use(express.urlencoded({extended:true}))
 
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'));
 
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
@@ -36,7 +38,8 @@ app.get('/campgrounds/new', (req,res)=>{
 app.post('/campgrounds', async (req,res)=>{
     const campground= new Campground(req.body.campground)
     await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)})
+    res.redirect(`/campgrounds/${campground._id}`)
+})
 
 app.get('/campgrounds/:id',async(req,res)=>{
     const campground =await Campground.findById(req.params.id);
@@ -44,7 +47,24 @@ app.get('/campgrounds/:id',async(req,res)=>{
 })
 
 
+app.get('/campgrounds/:id/edit',async(req,res)=>{
+    const campground =await Campground.findById(req.params.id);
+    res.render('campgrounds/edit',{campground})
+})
 
+app.put("/campgrounds/:id", async (req,res)=>{
+    const {id}= req.params
+     const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground})
+     res.redirect(`/campgrounds/${campground._id}`)
+
+    })
+
+app.delete("/campgrounds/:id",async(req,res)=>{
+     const {id}= req.params
+     const campground = await Campground.findByIdAndDelete({id})
+     res.redirect(`/campgrounds/index`)
+
+})
 
 app.listen(3000, ()=>{
     console.log("Server running on port 3000")
