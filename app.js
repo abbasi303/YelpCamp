34055003945ebb73d1fcbe7dea08a2f9397a75eb
Ussayed Shakeel <TTS72I@tr.pte.hu>
2023-08-06 14:44:38 +0200

    Create campgrounds

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ db.once("open",()=>{
 
 const app = express();
 
+app.use(express.urlencoded({extended:true}))
+
 
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
@@ -27,10 +29,22 @@ app.get('/campgrounds',async(req,res)=>{
     res.render('campgrounds/index',{campgrounds})
 })
 
+app.get('/campgrounds/new', (req,res)=>{
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req,res)=>{
+    const campground= new Campground(req.body.campground)
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)})
+
 app.get('/campgrounds/:id',async(req,res)=>{
     const campground =await Campground.findById(req.params.id);
     res.render('campgrounds/show',{campground})
 })
+
+
+
 
 app.listen(3000, ()=>{
     console.log("Server running on port 3000")
